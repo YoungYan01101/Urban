a51f4bdeb3a4d32dda996f33f9d3bbbdc2091656
YoungYan01101 <Yan01101@yandex.ru>
2024-12-01 17:38:11 +0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,11 @@
+# Переменные
+
+count_completed_tasks = 12
+count_spent_hours = 1.5
+name_course = 'Python'
+time_1_task = count_spent_hours / count_completed_tasks
+print('Курс ', name_course,
+      ', всего задач: ', count_completed_tasks,
+      ', затрачено часов: ', count_spent_hours,
+      ', среднее время выполнения: ', time_1_task, 'часа'
+      )
